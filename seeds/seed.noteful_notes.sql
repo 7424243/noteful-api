ed816d0b38cdf52ad03bcb93008118230c9e6e40
Sam Sheets <sam.sheets94@gmail.com>
2021-01-13 09:44:26 -0700

    created seed data for notes and folders

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_notes (note_name, content, folder_id)
+VALUES
+    ('Clean', 'clean room and bathroom', 1),
+    ('Groceries', 'grapes, bread, yogurt, chicken', 1),
+    ('Wind', 'there is a wind advisery for the next few days', 3)
--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -22,5 +22,47 @@ foldersRouter
             })
             .catch(next)
     })
+    // .post(jsonParser, (req, res, next) => {
+    //     const {folder_name} = req.body
+    //     const newFolder = folder_name
+    //     for (const [key, value] of Object.entries(newFolder))
+    //         if(value == null)
+    //             return res.status(400).json({
+    //                 error: {message: `Missing '${key}' in request body`}
+    //             })
+    //     FoldersService.insertFolder(
+    //         req.app.get('db'),
+    //         newFolder
+    //     )
+    //         .then(folder => {
+    //             res
+    //                 .status(201)
+    //                 .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+    //                 .json(serializeFolder(folder))
+    //         })
+    //         .catch(next)
+    // })
+
+foldersRouter
+    .route('/:folder_id')
+    .all((req, res, next) => {
+        FoldersService.getById(
+            req.app.get('db'),
+            req.params.folder_id
+        )
+            .then(folder => {
+                if(!folder) {
+                    return res.status(404).json({
+                        error: {message: `Folder doesn't exist`}
+                    })
+                }
+                res.folder = folder
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(serializeFolder(res.folder))
+    })
 
 module.exports = foldersRouter
