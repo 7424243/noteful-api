5aa4f025c3ef1b1ecf1fa981ef56089b3e24f0ed
Sam Sheets <sam.sheets94@gmail.com>
2021-01-13 09:17:02 -0700

    created makeMaliciousFolder

--- test/folders.fixtures.js
+++ test/folders.fixtures.js
@@ -15,6 +15,22 @@ function makeFoldersArray() {
     ]
 }
 
+function makeMaliciousFolder() {
+    const maliciousFolder = {
+        id: 911,
+        folder_name: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    }
+    const expectedFolder = {
+        ...maliciousFolder,
+        folder_name: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+    }
+    return {
+        maliciousFolder,
+        expectedFolder,
+    }
+}
+
 module.exports = {
     makeFoldersArray,
+    makeMaliciousFolder,
 }
