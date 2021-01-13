e076f6e2ba336d255f83c6dc9cb88a7504b8c951
Sam Sheets <sam.sheets94@gmail.com>
2021-01-13 08:37:39 -0700

    created makeNotesArray for testing

---
+++ test/notes.fixtures.js
@@ -0,0 +1,32 @@
+function makeNotesArray() {
+    return [
+        {
+            id: 1,
+            note_name: 'Note 1',
+            date_modified: '2029-01-22T16:28:32.615Z',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        },
+        {
+            id: 2,
+            note_name: 'Note 2',
+            date_modified: '2100-05-22T16:28:32.615Z',
+            content: 'Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+        },
+        {
+            id: 3,
+            note_name: 'Note 3',
+            date_modified: '1919-12-22T16:28:32.615Z',
+            content: 'Possimus, voluptate?',
+        },
+        {
+            id: 4,
+            note_name: 'Note 4',
+            date_modified: '1919-12-22T16:28:32.615Z',
+            content: 'Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam.',
+        },
+    ]
+}
+
+module.exports = {
+    makeNotesArray,
+}
