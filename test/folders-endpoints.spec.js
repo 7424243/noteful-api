991e490ed13526061db686f5b6b8feccc655473b
Sam Sheets <sam.sheets94@gmail.com>
2021-01-13 11:59:31 -0700

    implemented GET by folder id tests

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -2,6 +2,7 @@ const {expect} = require('chai')
 const knex = require('knex')
 const supertest = require('supertest')
 const app = require('../src/app')
+const foldersRouter = require('../src/folders/folders-router')
 const {makeFoldersArray, makeMaliciousFolder} = require('./folders.fixtures')
 
 describe('Folders endpoints', function() {
@@ -21,7 +22,7 @@ describe('Folders endpoints', function() {
     //clear up table after each test
     afterEach('cleanup', () => db.raw('TRUNCATE noteful_notes, noteful_folders RESTART IDENTITY CASCADE'))
 
-    describe.only('GET /api/folders', () => {
+    describe('GET /api/folders', () => {
         context('Given no folders', () => {
             it(`responds with 200 and an empty list`, () => {
                 return supertest(app)
@@ -55,6 +56,48 @@ describe('Folders endpoints', function() {
                     .expect(200)
                     .expect(res => {
                         expect(res.body[0].folder_name).to.eql(expectedFolder.folder_name)
+                    })
+            })
+        })
+    })
+
+    describe.only(`GET /api/folders/folder_id`, () => {
+        context(`Given no folders`, () => {
+            it('responds with 400', () => {
+                const folderId = 123456
+                return supertest(app)
+                    .get(`/api/folders/${folderId}`)
+                    .expect(404, {error: {message: `Folder doesn't exist`}})
+            })
+        })
+        context(`Given there are folders in the database`, () => {
+            const testFolders = makeFoldersArray()
+            beforeEach('insert folders', () => {
+                return db
+                    .into('noteful_folders')
+                    .insert(testFolders)
+            })
+            it('GET /api/folders:folder_id responds with 200 and the specified folder', () => {
+                const folderId = 2
+                const expectedFolder = testFolders[folderId - 1]
+                return supertest(app)
+                    .get(`/api/folders/${folderId}`)
+                    .expect(200, expectedFolder)
+            })
+        })
+        context('Given an XSS attack folder', () => {
+            const {maliciousFolder, expectedFolder} = makeMaliciousFolder()
+            beforeEach('insert folders', () => {
+                return db
+                    .into('noteful_folders')
+                    .insert(maliciousFolder)
+            })
+            it('removes XSS attack content', () => {
+                return supertest(app)
+                    .get(`/api/folders/${maliciousFolder.id}`)
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.body.folder_name).to.eql(expectedFolder.folder_name)
                     })
             })
         })
